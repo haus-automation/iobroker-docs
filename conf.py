31d448ed72d367a1d27c721852986dd25fd6a32d
Matthias Kleine <github.com9@emailu.de>
2021-10-12 12:56:00 +0200

    test

--- conf.py
+++ conf.py
@@ -17,6 +17,7 @@
 # sys.path.insert(0, os.path.abspath('.'))
 
 import sphinx_rtd_theme
+from sphinx.locale import _
 
 # -- Project information -----------------------------------------------------
 
