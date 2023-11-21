2174a8ecf654e2f564247e9d83c652941de1ac20
Matthias Kleine <github.com9@emailu.de>
2023-11-21 13:44:38 +0100

    License year

--- conf.py
+++ conf.py
@@ -22,7 +22,7 @@ from sphinx.locale import _
 # -- Project information -----------------------------------------------------
 
 project = u'ioBroker Dokumentation (UNOFFICIAL)'
-copyright = u'2022, Matthias Kleine (haus-automatisierung.com)'
+project_copyright = u'2023, Matthias Kleine (haus-automatisierung.com)'
 author = u'Matthias Kleine'
 
 # The short X.Y version
